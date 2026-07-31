b54b3e0594e42efa2c801f69f25135fc3d974392
Vitor Lassen <vitor@lassen.com.br>
2026-07-31 20:22:26 -0300

    polimorfismo

--- Cliente.cs
+++ Cliente.cs
@@ -6,44 +6,18 @@ namespace ScBank
     {
         public string End {  get;  set; }
       
-        public void Preencher()
+        public override void Preencher()
         {
-            Console.Write("Digite o nome do cliente: ");
-            Nome = Console.ReadLine();
-            Console.Write("Digite o telefone: "); 
-            Telefone = Console.ReadLine();
+            Console.WriteLine("Cadastro de Cliente");
+            PreencherPai();
             Console.WriteLine("Digite o Enderco: ");
             End = Console.ReadLine();
-
-            Console.WriteLine("Digite o email: ");
-            Email = Console.ReadLine();
-
-            while (!ValidarEmail())
-            {
-                Console.WriteLine("Email invalido Digite novamente: ");
-                Email = Console.ReadLine();
-            }
-
-
-            Console.WriteLine("Digite Cpf");
-            Cpf = Console.ReadLine();
-            while (!ValidarCpf(Cpf))
-            {
-                Console.WriteLine("Cpf Inválido digite novamente:");
-                Cpf = Console.ReadLine();
-            }
         }
 
-        public void Imprimir()
+        public override void Imprimir()
         {
-            Console.WriteLine("\n\n-----------------------");
-            Console.WriteLine("Nome: " +Nome);
-            Console.WriteLine("Cpf: "+ Cpf);
+            ImprimirPai();
             Console.WriteLine("Endereço: "+ End);
-            Console.WriteLine("Telefone: "+ Telefone);
-            Console.WriteLine("Email: "+ Email);
-            Console.WriteLine("----------------------\n\n");
-            
         }
     }
 }
